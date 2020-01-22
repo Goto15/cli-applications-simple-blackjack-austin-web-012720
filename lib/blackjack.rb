a005ebc25a86bb644f68082e606e302c53855512
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-22 22:03:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,9 +33,10 @@ def hit?(total)
   get_user_input
   
   if get_user_input = 'h'
+    deal_card
   elsif get_user_input = 's'
     display_card_total
-  else
+  elsif
     invalid_command
   end
   
